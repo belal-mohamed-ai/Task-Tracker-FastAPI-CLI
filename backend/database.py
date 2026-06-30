9aa6fe0d5a6607220137ab7ae14ff9b499f98791
Belal Mohamed <belal.mohammed.ntss@gmail.com>
2026-06-30 18:49:08 +0300

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = "sqlite:///./task_tracker.db"
+DATABASE_URL = "https://fforhdezinsaqdrmgmkr.supabase.co/rest/v1/student"
 
 engine = create_engine(
     DATABASE_URL,
